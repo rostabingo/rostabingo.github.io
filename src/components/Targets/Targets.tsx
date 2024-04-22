381c0d1054cdba0b686dc10f711139546f45b4b3
frisedel <frisedel@gmail.com>
2024-04-22 21:07:20 +0200

    add taget styles

--- src/components/Targets/Targets.tsx
+++ src/components/Targets/Targets.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
+import * as styles from "./Targets.module.scss";
+
 export const Targets: React.FC = () => {
     return (
         <div>
-            <div>
+            <div className={styles.target}>
                 <input type="checkbox" id="one" name="one" checked={false} />
                 <label htmlFor="one">En rad</label>
             </div>
-            <div>
+            <div className={styles.target}>
                 <input type="checkbox" id="two" name="two" />
                 <label htmlFor="two">Två rader</label>
             </div>
-            <div>
+            <div className={styles.target}>
                 <input type="checkbox" id="all" name="all" />
                 <label htmlFor="all">Hela brickan</label>
             </div>
